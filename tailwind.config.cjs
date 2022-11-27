f5e820d7dd49eaff90f712de57b10a50d9e17821
TijanaJelic <tijanajelic.jelic@gmail.com>
2022-11-27 12:41:49 +0100

    Added TailwindCSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,11 @@ module.exports = {
     extend: {
       colors: {
         primary: '#1B73E8',
+        lightGray: '#F7F7F7',
+        mediumGray: '#D4D4D4',
+        lightBlue: '#76A4FF',
+        darkBlue: '#276EF8',
+        green: '#049854',
       },
     },
   },
