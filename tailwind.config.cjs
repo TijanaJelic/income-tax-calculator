be3d3bbc0e0f9a2b23700f2d32efb38a7a1b33b4
TijanaJelic <tijanajelic.jelic@gmail.com>
2022-11-28 11:21:16 +0100

    Added new font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#1B73E8',
         lightGray: '#F7F7F7',
         mediumGray: '#D4D4D4',
         lightBlue: '#76A4FF',
         darkBlue: '#276EF8',
         green: '#049854',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
       },
     },
   },
